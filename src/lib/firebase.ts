29309a7f4255c09382b9f6b596f50a6cf927960e
lyriqondotsite <playesports900@gmail.com>
2025-08-11 12:31:21 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBXMrhGtxRubcHYWQzQRQgutknt9flgcf0",
-  authDomain: "learn-ai-37745.firebaseapp.com",
-  projectId: "learn-ai-37745",
-  storageBucket: "learn-ai-37745.firebasestorage.app",
-  messagingSenderId: "1091612791084",
-  appId: "1:1091612791084:web:2f7342be3535e83e9a9c89",
-  measurementId: "G-5RJYWPW07K"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
